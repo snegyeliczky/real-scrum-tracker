af96b802e60bd7933b00fd8c2a1e371e1a8e6478
sandornegyeliczky <sandor.negyeliczky@gmail.com>
2020-09-02 17:04:47 +0200

    dropdown companies and projects

--- src/components/CompanyWithProjects.js
+++ src/components/CompanyWithProjects.js
@@ -1,36 +1,52 @@
 import React, {useState} from 'react';
 import {CSSTransition} from "react-transition-group";
 import "../../src/assets/styles/dropDown.css";
+import ProjectCalls from "../services/ProjectCalls";
 
 const CompanyWithProjects = ({company}) => {
 
-    const [display,setDisplay] = useState(false);
+    const [display, setDisplay] = useState(false);
+    const [projects,setProjects] = useState();
 
     const toggle = () => {
         setDisplay(!display)
     };
 
-    return (
-            <div>
-            <div className="list_item " onClick={toggle}>name: {company.name}</div>
-                <div className={"sub-container"}>
-                < CSSTransition
-                    in={display}
-                    timeout={450}
-                    classNames="display"
-                    unmountOnExit
-                >
-                    < div
-                        className="submenu">
-                        <div className={"sublist"}>
-                            <div className="sublist-item" >hello</div>
-                        </div>
-                    </div>
-                </CSSTransition>
-                </div>
-            </div>
+    const getProjectFerCompany  =async () =>{
+        let r = await ProjectCalls.getProjectForCompany(company.id);
+        return r.data;
+    };
 
-    );
+    const showProjects = async () =>{
+        getProjectFerCompany()
+            .then(response =>{setProjects(response)})
+            .then(toggle)
+    }
+
+
+
+    return (
+        <div className={"s"}>
+            <div className="list_item " onClick={showProjects}>name: {company.name} id: {company.id}</div>
+            < CSSTransition
+                in={display}
+                timeout={345}
+                classNames="sub-display"
+                unmountOnExit
+            >
+                        <div className={"sublist"}>
+                            {projects!=null?projects.map(project =>{
+                                return <div className="sublist-item">{project.name}</div>
+                            }):'no projects'}
+
+                        </div>
+
+            </CSSTransition>
+</div>
+
+
+)
+    ;
 };
 
 export default CompanyWithProjects;
--- src/services/ProjectCalls.js
+++ src/services/ProjectCalls.js
@@ -12,6 +12,10 @@ export default {
     getCompanyForUser: async (userCredentials)=>{
         let response = await axios.post("http://localhost:8762/company-service/company-for-user",userCredentials);
         return response;
-    }
+    },
 
+    getProjectForCompany : async (companiId) =>{
+        let axiosResponse = await axios.get("http://localhost:8762/project-service/project/"+companiId);
+        return axiosResponse;
+    }
 }
